e5895aba69581b1c47a3f3520326e3da7fad0a00
jienhua <jien8013@hotmail.com>
2016-10-10 15:14:49 -0700

    first commit

---
+++ src/remote_action_middleware.js
@@ -0,0 +1,15 @@
+// export default function(store){
+// 	return function(next){
+// 		return function(action){
+
+// 		}
+// 	}
+// }
+
+
+export default socket=>store => next => action => {
+	if(action.meta && action.meta.remote){
+		socket.emit('action', action);
+	}
+	return next(action);
+}
